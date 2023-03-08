7d9511470be46d814c326a91585bd2fa6845df03
Mircea Dumitrescu <mirceagab@gmail.com>
2023-03-08 17:49:36 +0800

    functional global state manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { setData } from './store/actions/globalState';
+import { useSelector } from 'react-redux';
+
+
+interface GlobalState {
+  hasError: boolean,
+  data: string[],
+  isLoading: boolean
+}
 
 function App() {
-  return <div className="App">WORKS</div>;
+
+  const globalState = useSelector((state: GlobalState) => state)
+  const dispatch = useDispatch();
+  const [state, setState] = useState<GlobalState>({
+    data: [],
+    hasError: false,
+    isLoading: false
+  })
+
+  useEffect(() => {
+    setState(globalState.globalState)
+    console.log(state)
+  }, [globalState])
+
+  const addMockData = () => {
+    dispatch(setData({
+      data: ['vite', 'plus', 'react'],
+      isLoading: true,
+      hasError: false
+    }))
+  }
+
+  return <div className="App">
+    <button onClick={addMockData}>Add stuff to state</button>
+    <p>isLoading: {String(state.isLoading)}</p>
+    <p>hasError: {String(state.hasError)}</p>
+    {
+      state.data && state.data.map((el, idx) => {
+        return (
+          <p key={idx}>{el}</p>
+        )
+      }
+      )
+    }
+  </div>;
 }
 
 export default App;
