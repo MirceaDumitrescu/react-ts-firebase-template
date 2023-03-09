e2dfaee87f3dca67eb0dee483a7503dc62d02083
Mircea Dumitrescu <mirceagab@gmail.com>
2023-03-09 20:38:53 +0800

    replaced jest with vitest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import App from './App';
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 const initialState = {
   data: [],
   hasError: false,
   isLoading: false
-}
+};
 
 describe('App Rendering', () => {
   test('renders App page succesfully', () => {
@@ -23,8 +23,6 @@ describe('App Rendering', () => {
       </Router>
     );
 
-    expect(screen.getByText(/Save/i)).toBeDefined()
-
-
-  })
+    expect(screen.getByText(/Save/i)).toBeDefined();
+  });
 });
