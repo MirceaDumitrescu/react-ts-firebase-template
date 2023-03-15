8584737b75b60c038d643d8fa4949ddeb8216075
Mircea Dumitrescu <mirceagab@gmail.com>
2023-03-15 21:33:57 +0800

    added redux async thunk

--- src/store/actions/globalState.ts
+++ src/store/actions/globalState.ts
@@ -1,4 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { firestore } from '../../api/firebase/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+export interface GlobalState {
+  data: Partial<FirebaseResponse>;
+  isLoading: boolean;
+  hasError: boolean;
+}
+
+export interface FirebaseResponse {
+  payload: any;
+}
+
+export const fetchData = createAsyncThunk('data/fetchData', async () => {
+  try {
+    // some logic here (e.g fetch data from an API)
+    const querySnapshot = await getDocs(collection(firestore, 'test_data'));
+    querySnapshot.forEach((doc: any) => {
+      return { id: doc.id, ...doc.data() };
+    });
+    return querySnapshot;
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 const slice = createSlice({
   name: 'data',
@@ -18,6 +42,19 @@ const slice = createSlice({
       ...state,
       ...action.payload
     })
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchData.pending, (state, _) => {
+      state.isLoading = true;
+    }),
+      builder.addCase(fetchData.fulfilled, (state, action: Partial<FirebaseResponse>) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      }),
+      builder.addCase(fetchData.rejected, (state, _) => {
+        state.hasError = true;
+        state.isLoading = false;
+      });
   }
 });
 export default slice.reducer;
