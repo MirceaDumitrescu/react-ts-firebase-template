3faf1466673d00e5fa5f4916243e0b55d21a675e
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-03-17 10:17:57 +0200

    Implemented FormGenerator component + firebase auth

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,23 @@
+
+export interface FormConfig {
+    name: string;
+    type: string;
+    className: string;
+    value: string;
+    placeholder: string;
+    validation: object;
+}
+
+export interface CreateUserData {
+   firstName: string,
+   lastName: string,
+   displayName: string,
+   email: string,
+   password: string,
+   phone: string,
+}
+
+export interface LoginUserData {
+    email: string,
+    password: string,
+}
