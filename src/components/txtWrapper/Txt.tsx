ac3fce67a9bec28b6e9323884ab331ff7b1d7dd8
Mircea Dumitrescu <mirceagab@gmail.com>
2023-03-23 22:23:46 +0800

    fixed TXT wrapper

--- src/components/txtWrapper/Txt.tsx
+++ src/components/txtWrapper/Txt.tsx
@@ -4,46 +4,19 @@ interface Props
   extends React.HTMLAttributes<
     HTMLHeadingElement | HTMLSpanElement | HTMLDivElement | HTMLParagraphElement
   > {
-  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'span' | 'div' | 'p';
-  variant?:
-    | 'txt-1'
-    | 'txt-2'
-    | 'txt-3'
-    | 'txt-4'
-    | 'txt-5'
-    | 'txt-6'
-    | 'txt-7'
-    | 'hl-1'
-    | 'hl-2'
-    | 'hl-3'
-    | 'hl-4'
-    | 'hl-5'
-    | 'hl-7'
-    | 'hl-8'
-    | 'hl-9'
-    | 'hl-10'
-    | 'hl-11'
-    | 'field-label-1'
-    | 'field-label-2'
-    | 'small-text'
-    | 'link-1';
-  overwrite?: string;
-  dataTestId?: string;
+  as?: 'div' | 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  variant?: 'text-small' | 'text-large' | 'heading-small' | 'heading-large';
+  children?: React.ReactNode;
+  className?: string;
 }
 
 const Txt: FC<Props> = ({
   as: Component = 'div',
-  variant = 'hl-6',
-  overwrite = '',
+  variant = 'text-small',
   children,
-  className = '',
-  dataTestId = ''
+  className = ''
 }) => {
-  return (
-    <Component className={'test'} data-testid={dataTestId}>
-      {children}
-    </Component>
-  );
+  return <Component className={`${className}--${variant}`}>{children}</Component>;
 };
 
 export default Txt;
